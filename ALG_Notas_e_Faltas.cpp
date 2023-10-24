5180c49c8c0a8aad04ab39373e41d2d5fa7f06ec
Luciano Ferreira <lucianoferreira_rocha@outlook.com>
2023-10-23 21:28:49 -0300

    v0.3

--- ALG_Notas_e_Faltas.cpp
+++ ALG_Notas_e_Faltas.cpp
@@ -14,107 +14,87 @@ int main() {
     double n1, n2, n3, n4, n5, media, final, faltas;
     string nome, rgm;
 
-    cout << endl;
-    cout << "Por Favor, insira seu nome e sobrenome: \n";
+    cout << "\nPor Favor, insira seu nome e sobrenome: \n";
   	cout << "(exemplo: Joao_Silva)" << endl;
     cin >> nome;
-    cout << endl;
-    cout << "Insira seu numero de matricula: \n";
+    cout << "\nInsira seu numero de matricula: \n";
     cout << "(8 Digitos)"  << endl;
     cin >> rgm;
-    cout << endl;
-    cout << "Insira o numero de faltas:" << endl;
+    cout << "\nInsira o numero de faltas:" << endl;
     cin >> faltas;
     if (faltas < 15) {
-        cout << endl;
-        cout << "Insira sua nota 1" << endl;
+        cout << "\nInsira sua nota 1" << endl;
         cin >> n1;
-        cout << endl;
-        cout << "Insira sua nota 2" << endl;
+        cout << "\nInsira sua nota 2" << endl;
         cin >> n2;
-        cout << endl;
-        cout << "Insira sua nota 3" << endl;
+        cout << "\nInsira sua nota 3" << endl;
         cin >> n3;
-        cout << endl;
-        cout << "Insira sua nota 4" << endl;
+        cout << "\nInsira sua nota 4" << endl;
         cin >> n4;
         media = (n1 + n2 + n3 + n4) / 4;
         if (media < 4) {
-            cout << endl;
-            cout << "Aluno: " << nome << endl;
+            cout << "\nAluno: " << nome << endl;
             cout << "RGM: " << rgm << endl;
-            cout << endl;
-            cout << "Sua media final foi de: " << media << endl;
-            cout << endl;
+            cout << "\nSua media final foi de: " << media << endl;
             cout << "Status: Reprovado" << endl;
+            cout << "\nBoa sorte no ano que vem..." << endl;
             cout << endl;
-            cout << "Boa sorte no ano que vem..." << endl;
         } else {
             if (media >= 6) {
-                cout << endl;
-                cout << "Aluno: " << nome << endl;
+                cout << "\nAluno: " << nome << endl;
                 cout << "RGM: " << rgm << endl;
-                cout << "Sua media final foi de: " << media << endl;
+                cout << "\nSua media final foi de: " << media << endl;
                 cout << "Status: Aprovado" << endl;
+                cout << "\nBoas Ferias!!!!!!!!!" << endl;
                 cout << endl;
-                cout << "Boas Ferias!!!!!!!!!" << endl;
             } else {
-                cout << endl;
-                cout << "Sua media final foi de:" << media << endl;
-                cout << "Tera que fazer a prova de recuperacao!!!" << endl;
-                cout << endl;
-                cout << "Insira sua nota da avaliacao final:" << endl;
+                cout << "\nAluno: " << nome << endl;
+                cout << "RGM: " << rgm << endl;
+                cout << "\nSua media final foi de: " << media << endl;
+                cout << "\nStatus: Avaliacao Final" << endl;
+                cout << "\nInsira sua nota da avaliacao final:" << endl;
                 cin >> n5;
                 final = n5 / 2;
                 if (final < 2) {
-                    cout << endl;
-                    cout << "Aluno: " << nome << endl;
+                    cout << "\nAluno: " << nome << endl;
                     cout << "RGM: " << rgm << endl;
-                    cout << "Sua nota da prova final foi de: " << final << endl;
-                    cout << "Status: Reprovado" << endl;
+                    cout << "\nSua nota da prova final foi de: " << final << endl;
+                    cout << "Status: Reprovado pela avaliacao final" << endl;
+                    cout << "\nBoa sorte no ano que vem..." << endl;
                     cout << endl;
-                    cout << "Boa sorte no ano que vem..." << endl;
                 } else {
-                    cout << endl;
-                    cout << "Aluno: " << nome << endl;
+                    cout << "\nAluno: " << nome << endl;
                     cout << "RGM: " << rgm << endl;
-                    cout << "Sua nota da prova final foi de: " << final << endl;
+                    cout << "\nSua nota da prova final foi de: " << final << endl;
                     cout << "Status: Aprovado" << endl;
+                    cout << "\nBoas Ferias!!!!!!!!!" << endl;
                     cout << endl;
-                    cout << "Boas Ferias!!!!!!!!!" << endl;
                 }
             }
         }
     } else {
-        cout << endl;
-        cout << "Aluno: " << nome << endl;
+        cout << "\nAluno: " << nome << endl;
         cout << "RGM: " << rgm << endl;
-        cout << "Status: Reprovado pelo numero de faltas!!!" << endl;
-        cout << endl;
-        cout << "Para saber suas notas, insira sua Nota 1" << endl;
+        cout << "\nStatus: Reprovado pelo numero de faltas!!!" << endl;
+        cout << "\nPara saber suas notas, insira sua Nota 1" << endl;
         cin >> n1;
-        cout << endl;
-        cout << "Insira sua nota 2" << endl;
+        cout << "\nInsira sua nota 2" << endl;
         cin >> n2;
-        cout << endl;
-        cout << "Insira sua nota 3" << endl;
+        cout << "\nInsira sua nota 3" << endl;
         cin >> n3;
-        cout << endl;
-        cout << "Insira sua nota 4" << endl;
+        cout << "\nInsira sua nota 4" << endl;
         cin >> n4;
         media = (n1 + n2 + n3 + n4) / 4;
         if (media < 4) {
-            cout << endl;
-            cout << "Sua media final foi de: " << media << endl;
+            cout << "\nSua media final foi de: " << media << endl;
             cout << "Status: Reprovado pelas faltas e pelas notas." << endl;
+            cout << "\nBoa sorte no ano que vem..." << endl;
             cout << endl;
-            cout << "Boa sorte no ano que vem..." << endl;
         } else {
-            cout << endl;
-            cout << "Sua media final foi de: " << media << endl;
+            cout << "\nSua media final foi de: " << media << endl;
             cout << "Status: Aprovado pelas notas, porem reprovado pelas faltas." << endl;
+            cout << "\nBoa sorte no ano que vem..." << endl;
             cout << endl;
-            cout << "Boa sorte no ano que vem..." << endl;
         }
     }
     return 0;
